c4fbea7a41aa319903f7465682206d34bc5d9390
Savelyeva_A <Aliaksandra.Savelyeva@ibm.com>
2021-06-19 21:24:14 +0300

    practice_5 - riddles

--- riddles/urls.py
+++ riddles/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 app_name = 'riddles'
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^([0-9]+)/$', views.detail, name='detail'),
+    url(r'^([0-9]+)/answer/$', views.answer, name='answer')
 ]
--- riddles_project/settings.py
+++ riddles_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'riddles.apps.RiddlesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
